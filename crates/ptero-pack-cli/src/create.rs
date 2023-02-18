a2d506900c743f4b3dc2a569ecc7bf3cfb49fcc4
Layl Bongers <3094382+LaylBongers@users.noreply.github.com>
2023-02-18 16:45:07 +0100

    Add component data writing to test tool

--- crates/ptero-pack-cli/src/create.rs
+++ crates/ptero-pack-cli/src/create.rs
@@ -1,4 +1,8 @@
-use std::{fs::OpenOptions, io::Write, path::Path};
+use std::{
+    fs::OpenOptions,
+    io::{Seek, SeekFrom, Write},
+    path::Path,
+};
 
 use anyhow::{Context, Error};
 use clap::Args;
@@ -16,7 +20,12 @@ pub fn run(command: CreateCommand) -> Result<(), Error> {
 
     // This tool will in the future contain command line options for altering packages, but for
     // now is just a hardcoded test tool.
+    let test_data = include_bytes!("../../../data/shader.wgsl");
 
+    // Reserve 1kb for header and component table
+    let indices_offset: u32 = 1024;
+
+    // Open the target file, overwriting anything already there
     let mut file = OpenOptions::new()
         .write(true)
         .truncate(true)
@@ -32,8 +41,19 @@ pub fn run(command: CreateCommand) -> Result<(), Error> {
     header.set_count(1);
     file.write_all(header.as_bytes())?;
 
-    let entry = ComponentEntry::new(uuid!("2c5e4717-b715-429b-85cd-d320d242547a"));
+    let mut entry = ComponentEntry::new(uuid!("2c5e4717-b715-429b-85cd-d320d242547a"));
+
+    let mut data = [0u8; 8];
+    data[0..4].copy_from_slice(&indices_offset.to_le_bytes());
+    data[4..8].copy_from_slice(&(test_data.len() as u32).to_le_bytes());
+    entry.set_data(data);
+
     file.write_all(entry.as_bytes())?;
+
+    // Write the indices
+    file.seek(SeekFrom::Start(indices_offset as u64))?;
+    // TODO: Actually write indices, for now we'rEe just writing a single block of data in-place
+    file.write_all(test_data)?;
 
     Ok(())
 }
